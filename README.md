36f2e8a6a73c907d632c157207e2be097251255a
skakanka <kondracka.anna@gmail.com>
2015-10-22 19:41:19 -0400

    final commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# My Gallery is a custom jQuery gallery with thumbnails that brighten on hover and modal when clicked. Makes use of fancybox plugin.
---
+++ js/gallery.js
@@ -0,0 +1,20 @@
+$(function(){
+	var $thumbnails = $('#gallery img');
+
+	//thumbnail fading
+
+	$thumbnails.fadeTo(200, .5);
+
+	$thumbnails.hover(function(){
+		//mouseover
+		$(this).fadeTo(600, 1);
+	}, function(){
+		//mouseoff
+		$(this).fadeTo(400, .5);
+	
+	});
+
+	//image modal
+	$('#gallery').fancybox();
+
+});
